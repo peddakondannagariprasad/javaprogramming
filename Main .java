9c9194f87a263dd5f8adf8702a486a54d5023400
peddakondannagariprasad <peddakondannagariprasad1234@gmail.com>
2025-06-16 18:54:11 +0530

    Create Main .java

---
+++ Main .java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class Max
+
+{
+
+	public static void main(String[] args) {
+
+		int a[]=new int[5];
+
+		Scanner sc=new Scanner(System.in);
+
+		for(int i=0;i<a.length;i++){
+
+		   a[i]=sc.nextInt();
+
+		}
+
+		int m=0;
+
+		for(int i=0;i<a.length;i++){
+
+		    if(a[i]>m){
+
+		        m=a[i];
+
+		    }
+
+		}
+
+		System.out.println(m);
+
+	}
+
+}
