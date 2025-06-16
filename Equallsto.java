34296a64662618e37fab0892841b9f6678ea3de3
peddakondannagariprasad <peddakondannagariprasad1234@gmail.com>
2025-06-16 18:48:54 +0530

    Create Equallsto.java

---
+++ Equallsto.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Equalsto
+
+{
+
+	public static void main(String[] args) {
+
+		Scanner sc=new Scanner(System.in);
+
+		String a=sc.nextLine();
+
+		String b=sc.nextLine();
+
+		if(a.equals(b)){
+
+		    System.out.println(true);
+
+		}
+
+		else{
+
+		    System.out.println(false);
+
+		}
+
+		
+
+	}
+
+}
